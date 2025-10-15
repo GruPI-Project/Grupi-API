a6bde4e0eb35221ab1fd5363495a6c99aa738dcb
ALEXANDRE PAVANELLO E SILVA <apavanello@live.com>
2025-10-15 00:30:44 -0300

    Remove JWT token refresh endpoint from authentication URLs

--- GruPI/urls.py
+++ GruPI/urls.py
@@ -1,14 +1,12 @@
 # projeto/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenRefreshView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('core.urls')),
     path('api/v1/auth/', include('dj_rest_auth.urls')),
-    path('api/v1/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/auth/registration/', include('dj_rest_auth.registration.urls')),
 
     # Documentação da API
